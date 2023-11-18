94b4e62c4a4604264a9a1f1f58675f506b1c70c9
farhan <farhan.syaibir@gmail.com>
2023-11-18 22:33:01 +0700

    adding a completed and exit case

--- pythonProject/gui.py
+++ pythonProject/gui.py
@@ -7,9 +7,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values= function.get_todo(),key='todos',
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Completed")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                            layout=[[label], [input_box, add_button],[list_box, edit_button]],
+                            layout=[[label],
+                                    [input_box, add_button],
+                                    [list_box, edit_button, complete_button],
+                                    [exit_button]],
                             font=('Helvetica', 10))
 while True:
     event, values = window.read()
@@ -33,6 +38,17 @@ while True:
             todos[index] = new_todo
             function.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Completed":
+            todo_to_complete = values['todos'][0]
+            todos = function.get_todo()
+            todos.remove(todo_to_complete)
+            function.write_todos(todos)
+            window["todos"].update(values=todos)
+            window['todo'].update(value="")
+
+        case "Exit":
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
